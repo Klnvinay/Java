c42089cbb62b3e24ac9ed6c621273596f2b12819
Kotu Lakshmi Narasimha Vinay <113228693+Klnvinay@users.noreply.github.com>
2023-10-13 21:22:11 +0530

    Add files via upload

---
+++ Roman.java
@@ -0,0 +1,43 @@
+import java.util.HashMap;
+
+public class RomanToInteger {
+    public static int romanToInt(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+
+        // Create a HashMap to store the Roman numeral values
+        HashMap<Character, Integer> romanValues = new HashMap<>();
+        romanValues.put('I', 1);
+        romanValues.put('V', 5);
+        romanValues.put('X', 10);
+        romanValues.put('L', 50);
+        romanValues.put('C', 100);
+        romanValues.put('D', 500);
+        romanValues.put('M', 1000);
+
+        int result = 0;
+        int prevValue = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char currentChar = s.charAt(i);
+            int currentValue = romanValues.get(currentChar);
+
+            if (currentValue < prevValue) {
+                result -= currentValue;
+            } else {
+                result += currentValue;
+            }
+
+            prevValue = currentValue;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String romanNumeral = "IX"; // Example: IX = 9
+        int integerEquivalent = romanToInt(romanNumeral);
+        System.out.println("The integer equivalent of " + romanNumeral + " is " + integerEquivalent);
+    }
+}
