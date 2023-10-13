4a07bf91ef5a258d33dc8fa0f1aa5c81c0603d48
Kotu Lakshmi Narasimha Vinay <113228693+Klnvinay@users.noreply.github.com>
2023-10-13 21:27:37 +0530

    Add files via upload

---
+++ reverse.js
@@ -0,0 +1,19 @@
+function reverseWordsInSentence(sentence) {
+    // Split the sentence into words
+    const words = sentence.split(' ');
+
+    // Reverse each word and store them in a new array
+    const reversedWords = words.map(word => {
+        // Use split, reverse, and join to reverse the word
+        return word.split('').reverse().join('');
+    });
+
+    // Join the reversed words to form the reversed sentence
+    const reversedSentence = reversedWords.join(' ');
+
+    return reversedSentence;
+}
+
+const inputSentence = "This is a sunny day";
+const reversedSentence = reverseWordsInSentence(inputSentence);
+console.log(reversedSentence); // Output: "sihT si a ynnus yad"
